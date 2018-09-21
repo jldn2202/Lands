5f245f216020eb4e07cc7a301cd50f120ed55231
jldn2202 <43429407+jldn2202@users.noreply.github.com>
2018-09-20 20:37:41 -0500

    Actualizado al 2018

---
+++ Lands/Lands/Infraestructure/InstanceLocator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lands.Infraestructure
+{
+    using ViewModels;
+
+   
+    public class InstanceLocator
+    {
+        #region Porperties
+        public MainViewModel Main
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructors
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+        #endregion
+    }
+}
---
+++ Lands/Lands/ViewModels/LoginViewModel.cs
@@ -0,0 +1,52 @@
+﻿
+
+namespace Lands.ViewModels
+{
+    using System.Windows.Input;
+
+    public class LoginViewModel
+    {
+        #region Properties
+        public string Email
+        {
+            get;
+            set;
+        }
+        public string Password
+        {
+            get;
+            set;
+        }
+        public bool IsRunnning
+        {
+            get;
+            set;
+        }
+        public bool IsRememberd
+        {
+            get;
+            set;
+        }
+
+
+        #endregion
+
+        #region Constructors
+        public LoginViewModel()
+            {
+            this.IsRememberd = true;
+            }
+        #endregion
+
+
+
+        #region Commands
+        public ICommand LoginCommand
+        {
+            get;
+            set;
+        }
+        #endregion
+
+    }
+}
